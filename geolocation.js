2cd1ce11e9034449494a7848fbfa5c49212d31fd
aldompe95 <aldompe95@gmail.com>
2017-03-09 15:55:57 -0600

    delete unnecessary function

--- geolocation.js
+++ geolocation.js
@@ -19,7 +19,7 @@ function initMap(){
 
   // This event listener calls addMarker() when the map is clicked.
   google.maps.event.addListener(map, 'click', function(event){
-    if(countMarkersInMap < maxMarkersInMap){ // add the if so the user can set more than 3 markers
+    if(countMarkersInMap < maxMarkersInMap){ // user cant set more than 3 markers
       addMarker(event.latLng, map);
       countMarkersInMap++;
     }else{
@@ -38,7 +38,7 @@ function addMarker(location, map){
     map: map
   });
   markersCoordinates.push(location); // Save the direction of the marker
-  markers.push(marker);
+  markers.push(marker); // Save the marker
 }
 
 // Deletes all markers in the array by removing references to them.
@@ -61,7 +61,7 @@ function printDistance(){
     html += `<li> Marker A to marker C: ${getDistanceFromLatLonInKm(markersCoordinates[0].lat(),markersCoordinates[0].lng(),markersCoordinates[2].lat(),markersCoordinates[2].lng())}KM</li>`;
     html += `<li> Marker A to marker C: ${getDistanceFromLatLonInKm(markersCoordinates[1].lat(),markersCoordinates[1].lng(),markersCoordinates[2].lat(),markersCoordinates[2].lng())}KM</li>`;
     document.querySelector('#output').innerHTML = html;
-    setCoordinatesDrawing();
+    markersCoordinates.push(markersCoordinates[0]);
     drawLines();
   }else{
     alert("You must select at least 3 locations");
@@ -88,24 +88,14 @@ function deg2rad(deg){
 }
 
 // DrawLines feature
-let path;
-
 function drawLines(){
+  markersCoordinates.push(markersCoordinates[0]);
   let line = new google.maps.Polyline({
-    path: path,
+    path: markersCoordinates,
     geodesic: true,
     strokeColor: '#FF0000',
     strokeOpacity: 1.0,
     strokeWeight: 2
   });
-  console.log(line);
   line.setMap(map);
-}
-
-function setCoordinatesDrawing(){
-  path = [];
-  for (let i = 0; i < markersCoordinates.length; i++){
-    path.push({lat: markersCoordinates[i].lat(), lng: markersCoordinates[i].lng()});
-  }
-  path.push({lat: markersCoordinates[0].lat(), lng: markersCoordinates[0].lng()});
 }

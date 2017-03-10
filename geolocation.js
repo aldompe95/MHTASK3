e755fa5fcb31efdc0d9e3c25da61dda2c089bc07
aldompe95 <aldompe95@gmail.com>
2017-03-10 17:51:01 -0600

    change harvesine for google api function

--- geolocation.js
+++ geolocation.js
@@ -53,38 +53,29 @@ function deleteMarkers() {
 
 google.maps.event.addDomListener(window, 'load', initMap);
 
+let tempArray = [];
+var countt =0;
+let isAlready = 0;
 // Distance feature
 function printDistance(){
-  if(markersCoordinates.length == 3){
+  if(markersCoordinates.length >= 3){
     let html;
-    html = `<li> Marker A to marker B: ${getDistanceFromLatLonInKm(markersCoordinates[0].lat(),markersCoordinates[0].lng(),markersCoordinates[1].lat(),markersCoordinates[1].lng())}KM</li>`;
-    html += `<li> Marker A to marker C: ${getDistanceFromLatLonInKm(markersCoordinates[0].lat(),markersCoordinates[0].lng(),markersCoordinates[2].lat(),markersCoordinates[2].lng())}KM</li>`;
-    html += `<li> Marker A to marker C: ${getDistanceFromLatLonInKm(markersCoordinates[1].lat(),markersCoordinates[1].lng(),markersCoordinates[2].lat(),markersCoordinates[2].lng())}KM</li>`;
+    html = `<li> Marker A to marker B: ${getDistanceFromLatLonInKm(markersCoordinates[0],markersCoordinates[1])}KM</li>`;
+    html += `<li> Marker A to marker C: ${getDistanceFromLatLonInKm(markersCoordinates[0],markersCoordinates[2])}KM</li>`;
+    html += `<li> Marker A to marker C: ${getDistanceFromLatLonInKm(markersCoordinates[1], markersCoordinates[2])}KM</li>`;
     document.querySelector('#output').innerHTML = html;
-    markersCoordinates.push(markersCoordinates[0]);
+    markersCoordinates.push(markersCoordinates[0]); // to close the triangle drawing
     drawLines();
   }else{
     alert("You must select at least 3 locations");
   }
 }
-  
-// Haversine formula
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2){
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d;
-}
 
-function deg2rad(deg){
-  return deg * (Math.PI/180)
+function getDistanceFromLatLonInKm(pointA, pointB){
+  let distanceInM = google.maps.geometry.spherical.computeDistanceBetween(pointA, pointB);
+  const metersInKm = 1000; 
+  let distanceInKm = distanceInM / metersInKm;
+  return distanceInKm;
 }
 
 // DrawLines feature
